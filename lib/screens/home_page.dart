282d588b2d33f8ad98871463f28174e93d689e01
Dylankavvundama <85756267+dylankavundama@users.noreply.github.com>
2024-05-21 16:57:43 +0200

    refact

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -40,7 +40,7 @@ class _Home_PageState extends State<Home_Page> {
       ),
       // floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       // floatingActionButton: const CustomFloatingbutton(),
-      //   appBar: AppBar(),
+
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
