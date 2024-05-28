c0dc3f48b24dc5e8b62d735611391bc41f7eb49d
baruka99 <baruka99.github@gmail.com>
2024-05-28 20:26:48 +0200

    fixing main ChangeNotifierProvider bug

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:solo_wallet/services/app_setting.dart';
 import 'package:solo_wallet/services/shared_prefs.dart';
 import 'package:solo_wallet/views/start/solo_wallet.dart';
 
 Future<void> main() async {
   AppPref.prefs = await SharedPreferences.getInstance();
 
-  runApp(const SoloApp());
+  runApp(
+    ChangeNotifierProvider<AppSetting>(
+      create: (context) => AppSetting(),
+      child: Consumer<AppSetting>(
+        builder: (context, value, child) => const SoloApp(),
+      ),
+    ),
+  );
 }
