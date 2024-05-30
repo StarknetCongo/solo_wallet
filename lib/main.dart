0e299d4039e2b3480eea501377f0fb9d8b90c257
Dylankavvundama <85756267+dylankavundama@users.noreply.github.com>
2024-05-30 22:27:34 +0200

    add qr_flutter

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Solo Wallet',
       theme: ThemeData(primarySwatch: Colors.blue, fontFamily: 'Google'),
-      home: Loading(),
+      home: const Loading(),
     );
   }
 }
