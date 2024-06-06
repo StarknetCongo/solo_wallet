ca0e3a2af1177ca96351ad98b9d9b92f616fcb81
kanane cedric <bahakanane@gmail.com>
2024-06-06 16:06:00 +0200

    custom pin textfield

--- lib/views/components/textfield.dart
+++ lib/views/components/textfield.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:solo_wallet/services/extentions.dart';
 import 'package:solo_wallet/services/style.dart';
 
@@ -60,6 +61,79 @@ class CustomTextField extends StatelessWidget {
           ),
         ),
       ],
+    );
+  }
+}
+
+class CustomPinTextField extends StatefulWidget {
+  final String? hintText;
+  final String? labelText;
+  final TextEditingController? controller;
+  final TextInputType? keybordType;
+  final bool autofocus;
+  const CustomPinTextField(
+      {super.key,
+      this.controller,
+      this.hintText,
+      this.labelText,
+      this.autofocus = false,
+      this.keybordType});
+
+  @override
+  State<CustomPinTextField> createState() => _CustomPinTextFieldState();
+}
+
+class _CustomPinTextFieldState extends State<CustomPinTextField> {
+  ValueNotifier<bool> isObscure = ValueNotifier(true);
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+      valueListenable: isObscure,
+      builder: (context, bool obscureText, child) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "${widget.labelText}",
+              style: const TextStyle(
+                fontWeight: FontWeight.normal,
+                fontSize: 13,
+              ),
+            ),
+            AppStyle.SPACING_XS.heightBox,
+            Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).disabledColor,
+                borderRadius: BorderRadius.circular(AppStyle.RADIUS_MD),
+              ),
+              child: TextField(
+                cursorColor: Theme.of(context).primaryColor,
+                autofocus: widget.autofocus,
+                obscureText: obscureText,
+                controller: widget.controller,
+                keyboardType: widget.keybordType,
+                obscuringCharacter: '●',
+                decoration: InputDecoration(
+                  isDense: true,
+                  contentPadding: const EdgeInsets.all(16),
+                  border: InputBorder.none,
+                  hintText: widget.hintText,
+                  hintStyle: Theme.of(context).textTheme.bodySmall,
+                  suffix: InkWell(
+                    onTap: () {
+                      isObscure.value = !isObscure.value;
+                    },
+                    child: Icon(
+                      obscureText ? Iconsax.eye : Iconsax.eye_slash,
+                      size: 16,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
