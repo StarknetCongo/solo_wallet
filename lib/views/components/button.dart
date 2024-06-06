feec011621a46550234e0cc9a83b99b824e15a5b
kanane cedric <bahakanane@gmail.com>
2024-06-06 16:03:12 +0200

    custom outlinebutton

--- lib/views/components/button.dart
+++ lib/views/components/button.dart
@@ -53,3 +53,56 @@ class CustomButton extends StatelessWidget {
     );
   }
 }
+
+class OutlineCustomButton extends StatelessWidget {
+  final String? title;
+  final Color? titleColor;
+  final Color? backGroundColor;
+  final Function()? onTap;
+
+  final double? width;
+  final AppState? state;
+  const OutlineCustomButton({
+    super.key,
+    required this.title,
+    this.titleColor,
+    this.backGroundColor,
+    this.onTap,
+    this.width,
+    this.state,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: AnimatedContainer(
+        width: width,
+        duration: const Duration(milliseconds: 200),
+        alignment: Alignment.center,
+        padding: const EdgeInsets.symmetric(
+          vertical: 16,
+          horizontal: 16,
+        ),
+        decoration: BoxDecoration(
+          border: Border.all(color: Theme.of(context).dividerColor),
+          borderRadius: BorderRadius.circular(AppStyle.RADIUS_MD),
+        ),
+        child: state is LOADING
+            ? SizedBox(
+                height: 15,
+                width: 15,
+                child: CircularProgressIndicator(
+                    color: Theme.of(context)
+                        .progressIndicatorTheme
+                        .circularTrackColor),
+              )
+            : Text(
+                "$title",
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+      ),
+    );
+  }
+}
